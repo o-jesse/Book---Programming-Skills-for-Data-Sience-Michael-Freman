6bfca9f40b84e53f86b387ad50a1a62ef074b88c
o-jesse <jesse.o.au@gmail.com>
2021-11-08 07:52:13 +1100

    Add chapter 11 practice reading section.

---
+++ Chpater 11.R
@@ -0,0 +1,123 @@
+library(dplyr)
+install.packages("pscl")
+library(pscl)
+View(presidentialElections)
+
+votes <- select(presidentialElections, year, demVote)
+View(votes)
+
+votes_baser <- presidentialElections[,c("year","demVote")]
+View(votes_baser)
+
+View(
+  select(
+    presidentialElections, state:year
+  )
+)
+
+View(
+  select(
+    presidentialElections,
+    -south
+  )
+)
+
+votes_2008 <- filter(
+  presidentialElections,
+  year == 2008
+)
+View(votes_2008)
+
+votes_2008_baser <- presidentialElections[presidentialElections$year == 2008,]
+View(votes_2008_baser)
+
+votes_colorado_2008 <- filter(
+  presidentialElections,
+  state == "Colorado",
+  year == 2008
+)
+View(votes_colorado_2008)
+
+#demVote = votes for Democrats
+
+presidentialElections <- mutate(
+  presidentialElections,
+  other_parties_vote = 100 - demVote,
+  abs_vote_diff = abs(demVote - other_parties_vote) #abs is not necessary. It could have been var1 - var2 only.
+)
+
+# testing below why used abs(). Using abs() was not actually necessary in the example. Commenting out everything to row 66
+# other_parties_vote <- 100 - presidentialElections$demVote
+# absence <- presidentialElections$demVote - other_parties_vote
+# abs_book <- abs(presidentialElections$demVote - other_parties_vote)
+# 
+# votes_analysis <- matrix(
+#   c(
+#     presidentialElections$demVote,
+#     other_parties_vote,
+#     absence,
+#     abs_book
+#   ),
+#   ncol = 4,
+#   byrow = FALSE,
+#   dimnames = list(NULL,c("demVote","other_parties_votes","absence","abs_book"))
+# )
+# 
+# View(votes_analysis)
+
+View(presidentialElections)
+
+presidentialElections <- arrange(presidentialElections,-year,demVote)
+View(presidentialElections)
+
+average_votes <- summarise(
+  presidentialElections,
+  mean_demVote = mean(demVote),
+  mean_other_parties = mean(other_parties_vote)
+)
+average_votes
+
+furthest_from_50 <- function(vec) {
+  adjusted_values <- vec - 50
+  
+  vec[adjusted_values == max(adjusted_values)]
+}
+
+furthest_from_50(presidentialElections[,2])
+
+presidentialElections[
+  presidentialElections$demVote > 98,
+]
+
+View(
+  summarise(
+    presidentialElections,
+    biggest_landslide=furthest_from_50(demVote)
+  )
+)
+
+votes_2008 <- filter(presidentialElections, year == 2008)
+View(votes_2008)
+
+most_dem_votes <- filter(votes_2008,demVote == max(demVote))
+View(most_dem_votes)
+
+most_dem_state <- select(most_dem_votes,state)
+View(most_dem_state)
+
+most_dem_state2 <-
+  
+  
+  
+
+
+
+
+
+
+
+
+
+
+
+
